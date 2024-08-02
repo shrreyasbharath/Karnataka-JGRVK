82ba1a9503bc7c0ee5de2a9a3656ecff4a4ba6e2
shrreyasbharath <85285429+shrreyasbharath@users.noreply.github.com>
2024-08-02 18:34:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = e.target.getAttribute('href').substring(1);
+            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
